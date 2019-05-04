bd0cd170eabb0f290df08e03b50296851f68bc8f
Rhuan Carlos <rhcarlosweb@gmail.com>
2019-05-04 15:18:38 -0300

    initial

---
+++ ACFIcoMoonPlugin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+	Plugin Name: Advanced Custom Fields: ACF IcoMoon
+	Plugin URI: https://github.com/rhcarlosweb/acf-icomoon-select
+	Description: Show IcoMoon select field based on selection.json generated from IcoMoon App
+	Version: 0.0.1-alpha
+	Author: Rhuan Carlos
+	Author URI: https://rhuancarlos.com
+	License: GPLv2 or later
+	License URI: http://www.gnu.org/licenses/gpl-2.0.html
+	Text Domain: rhicomoon
+*/
